9402ffff53759f292c9c0fe70db7f35ebac38fdc
KathyPuma <kathypuma96@gmail.com>
2020-06-20 23:55:43 -0400

    Create queries for users

--- server/queries/users.js
+++ server/queries/users.js
@@ -1,4 +1,36 @@
 const db = require('../database/db')
 
 
-module.exports = {};
+const getAllUsers = async () => {
+	const users = await db.any("SELECT * FROM users")
+	return users;
+}
+
+const addNewUser = async (userObj) => {
+	const newUserQuery = `
+		INSERT INTO users(username, password)
+			VALUES($/username/, $/password/)
+			RETURNING user_id, username
+	`
+	const newUser = await db.one(newUserQuery, userObj)
+	return newUser;
+}
+
+const getUserByUsername = async (username) => {
+	const getUserQuery = 
+	`SELECT * 
+	FROM users
+	WHERE username = $/username/;
+	`;
+	return await await db.oneOrNone(getUserQuery, {username})
+
+
+
+}
+
+module.exports = {
+	getAllUsers,
+	addNewUser,
+	getUserByUsername
+}
+
