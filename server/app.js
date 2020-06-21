81ecd0cb273bc9514050dfeeda18c32dd27a594a
KathyPuma <kathypuma96@gmail.com>
2020-06-21 00:05:51 -0400

    Declare variable for session and passport and create route for auth route

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-
+const session = require('express-session');
+const passport = require('./auth/passport');
+const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -13,7 +14,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: "NOT_A_GOOD_SECRET",
+    resave: false,
+    saveUninitialized: true
+}))
 
+app.use(passport.initialize()) 
+app.use(passport.session())
+
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 
 
