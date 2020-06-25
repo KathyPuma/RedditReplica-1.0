6ec57d72eabcd99870af24b4050ea17779e53ee5
KathyPuma <kathypuma96@gmail.com>
2020-06-24 23:01:07 -0400

    Create routes for subreddit and votes and import cors

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const session = require('express-session');
 const passport = require('./auth/passport');
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
+const subredditRouter = require('./routes/subreddit')
+const votestRouter = require('./routes/votes')
 
 const app = express();
 
@@ -14,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.use(session({
     secret: "NOT_A_GOOD_SECRET",
     resave: false,
@@ -25,6 +29,8 @@ app.use(passport.session())
 
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
+app.use('/subreddit',subredditRouter)
+app.use('/votes',votestRouter)
 
 
 module.exports = app;
