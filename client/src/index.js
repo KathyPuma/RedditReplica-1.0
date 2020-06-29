06c72660f8116f939eb8f42987ea9aa2e6aff0e6
KathyPuma <kathypuma96@gmail.com>
2020-06-28 22:06:27 -0400

    Import and Wrap Provider component around App which will give App.js access to the store

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store/store'
 import App from './App';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
 
 
