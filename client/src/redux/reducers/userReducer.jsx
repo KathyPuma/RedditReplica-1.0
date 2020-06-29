7215f211f4f3a19012c1f7e2a01782bd5aa3294c
KathyPuma <kathypuma96@gmail.com>
2020-06-28 22:10:48 -0400

    Create redux reducers that update state which holds a logged in user's id, username and email

---
+++ client/src/redux/reducers/userReducer.jsx
@@ -0,0 +1,8 @@
+export default function userReducer(state = { user_id: null ,username: '',email:''  }, action) {
+    switch (action.type) {
+    //    case 'LOGIN':
+    //       return { };
+       default:
+          return state
+    }
+ }
