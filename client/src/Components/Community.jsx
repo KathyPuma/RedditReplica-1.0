efc8f91d7537ee04194cc0d8019036faca7e24c5
KathyPuma <kathypuma96@gmail.com>
2020-06-29 23:49:29 -0400

    Community page that will make a network request to the backend and retrieve all post from a specific subreddit and send the information to CommunityPage.jsx as a prop

--- client/src/Components/Community.jsx
+++ client/src/Components/Community.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import CommunityPage from './CommunityPage'
 import axios from 'axios';
 
 
 
-function Community() {
+function Community(props) {
+    const [subreddit, setSubreddit] = useState([]);
+
+    useEffect(() => {
+        const handleAllSubReddits = async () => {
+            const allSubreddits = await axios.get(`/subreddit/name/${props.match.params.community}`)
+            setSubreddit(allSubreddits.data.payload)
+            console.log("allSubreddits.data.payload", allSubreddits.data.payload)
+
+        }
+        handleAllSubReddits()
+    }, [])
+
+
     return (
         <div className="community-stage" >
+
+            <CommunityPage
+                subreddit={subreddit}
+            />
+
         </div >
     )
 }
