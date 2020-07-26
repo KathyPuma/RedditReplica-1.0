f21603d98744ce4f5d9a17915e1ae36e9dfb5c3e
KathyPuma <kathypuma96@gmail.com>
2020-07-25 22:25:52 -0400

    Display all subreddit posts on the home page

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './Home.css'
+
+function Home(props) {
+  const [subredditsPosts, setSubredditsPosts] = useState([]);
+
+  useEffect(() => {
+    const handleAllSubReddits = async () => {
+      const allSubreddits = await axios.get(`/subreddit`)
+      setSubredditsPosts(allSubreddits.data.payload)
+      console.log("allSubreddits.data.payload", allSubreddits.data.payload)
+    }
+    handleAllSubReddits()
+  }, [])
 
 
 
-function Home() {
-    return (
-        <div className="home-stage"  >   
-        
-        </div >
-    )
+  return (
+    <div className="home-stage"  >
+      {subredditsPosts.map(posts => {
+        return (
+          <div className="subreddit-card">
+            <div className='card-words'>
+
+              <Link to={`/r/${posts.subreddit_name}`}>
+                <span>r/{posts.subreddit_name} </span>
+              </Link>
+              <span>posted by
+                             <div>  <Link to={`/user/${posts.username}`}>
+                  <span>
+                    {posts.username}
+                  </span>
+                </Link>
+                </div>
+              </span>
+            </div>
+
+
+            <span className='card-title'>{posts.title}</span>
+            <h2>{posts.body}</h2>
+            <img className='subreddit-card-img' src={posts.photo_url} />
+
+          </div>
+        )
+      })}
+
+
+
+    </div >
+  )
 }
+
 export default Home;
-
-
-
-
-
 
 
