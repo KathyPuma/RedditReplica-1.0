3c62e93e1490b947a59b7453a3e5ecb12ab691ea
KathyPuma <kathypuma96@gmail.com>
2020-07-26 23:26:45 -0400

    Modify the getAllPostBySubbreddit by joining votes_posts table to get the sum of votes per subreddit post

--- server/queries/subredditPosts.js
+++ server/queries/subredditPosts.js
@@ -2,17 +2,18 @@ const db = require('../database/db')
 
 const getAllPostBySubbreddit = async (id) => {
     const getAllPostBySubReddit =
-        `
-        SELECT  
-        subreddit.subreddit_name, users.username,
-        subreddit_posts.subreddit_posts_id, subreddit_posts.title , subreddit_posts.body  , subreddit_posts.photo_url ,
-        subreddit_posts.time_post, SUM(votes_posts.votes)
-        FROM subreddit_posts 
-        JOIN subreddit ON subreddit.subreddit_id = subreddit_posts.subreddit_id
-        JOIN users ON users.user_id = subreddit_posts.poster_id
-        JOIN votes_posts ON  votes_posts.subreddit_posts_id = subreddit_posts.subreddit_posts_id
-        GROUP BY subreddit.subreddit_name
-        `
+    `
+    SELECT  
+    subreddit.*, users.username,
+    subreddit_posts.subreddit_posts_id, subreddit_posts.title , subreddit_posts.body  , subreddit_posts.photo_url ,
+    subreddit_posts.time_post, sum(votes_posts.votes) AS vote_total
+    FROM subreddit_posts 
+    JOIN subreddit ON subreddit.subreddit_id = subreddit_posts.subreddit_id
+    JOIN users ON users.user_id = subreddit_posts.poster_id
+    FULL JOIN votes_posts ON  votes_posts.subreddit_posts_id = subreddit_posts.subreddit_posts_id
+    GROUP BY subreddit.subreddit_id,users.username,subreddit_posts.subreddit_posts_id,
+    votes_posts.votes;
+    `
     return await db.any(getAllPostBySubReddit)
 }
 
