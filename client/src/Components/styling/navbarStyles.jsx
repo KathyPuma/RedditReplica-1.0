5ed3f74862af4297ef6a9d7e94429e49c340aef3
KathyPuma <kathypuma96@gmail.com>
2020-07-26 23:41:05 -0400

    Create custom style for navbar

---
+++ client/src/Components/styling/navbarStyles.jsx
@@ -0,0 +1,104 @@
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
+import customTheme from './customTheme';
+import Badge from '@material-ui/core/Badge';
+
+
+
+export const navbarStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  logo: {
+    height: '50px'
+  },
+  appBar: {
+    backgroundColor:'white',
+    '.MuiAppBar-colorPrimary': {
+      color: customTheme.palette.secondary.dark
+    },
+    '.MuiSvgIcon-root': {
+      color: customTheme.palette.secondary.dark
+    },
+  },
+  customerMenu: {
+    color: customTheme.palette.secondary.dark,
+    'a:link': {
+      textDecoration: 'none'
+    }
+  },
+  iconStyling: {
+    color: customTheme.palette.secondary.dark
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    textDecoration: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+
+}));
+
+
+export const StyledBadge = withStyles((theme) => ({
+  badge: {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}))(Badge);
