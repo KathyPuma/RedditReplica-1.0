f0a2ecf415dcff5c250c5fdfb4fac034107479cb
KathyPuma <kathypuma96@gmail.com>
2020-08-03 22:56:23 -0400

    Create reducer for user when they attempt to register

--- client/src/redux/reducers/userReducer.jsx
+++ client/src/redux/reducers/userReducer.jsx
@@ -1,8 +1,41 @@
-export default function userReducer(state = { user_id: null ,username: '',email:''  }, action) {
+import {
+    REGISTER_REQUEST,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from '../actions/actionTypes'
+
+const initialState = {
+    loggedIn: false,
+    user: [],
+    message: ''
+}
+
+const userReducer = (state = initialState, action) => {
+
     switch (action.type) {
-    //    case 'LOGIN':
-    //       return { };
-       default:
-          return state
+        case REGISTER_REQUEST:
+            return {
+                ...state,
+            }
+        case REGISTER_SUCCESS:
+            return {
+                loggedIn: true,
+                user: action.payload.payload,
+                message: action.payload.message
+
+            }
+        case REGISTER_FAIL:
+            return {
+                loggedIn: false,
+                user: [],
+                message: action.payload
+
+            }
+        default: return state
     }
- }
+}
+
+export default userReducer
+
+
+
