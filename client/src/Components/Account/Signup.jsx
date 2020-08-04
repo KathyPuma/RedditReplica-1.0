dd4457d06eb7c9d744d63880188651ac0fceee15
KathyPuma <kathypuma96@gmail.com>
2020-08-03 23:39:58 -0400

    Create a signup form and send form details to registerUser function where the function will update redux state

--- client/src/Components/Account/Signup.jsx
+++ client/src/Components/Account/Signup.jsx
@@ -1,13 +1,112 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { registerUser } from '../../redux/actions/userActions'
 
 
+function Signup({ user, registerUser }) {
+    const [newUser, setNewUser] = useState({
+        username: '',
+        email: '',
+        password: '',
+        avatar_url: '',
+    })
 
-function Signup() {
+    const handleRegister = () => {
+        registerUser(newUser)
+    }
+
     return (
-        <div className="signup-stage" >
-        </div >
-    )
+        <div className='signupPage'>
+
+            <div className='signup-stage'>
+                <div className={'singup-button'}>
+                    <div className='register'>Register</div>
+
+
+                    <div className='username-div'>
+                        <input
+                            className='username-input'
+                            type='text' name='username'
+                            id='username'
+                            placeholder='Username'
+                            onChange={e => {
+                                let usernameInput = e.target.value
+                                setNewUser(prevState => {
+                                    return { ...prevState, username: usernameInput }
+                                })
+                            }}
+                        />
+                    </div>
+
+                    <div className='email-div'>
+                        <input
+                            className='email-input'
+                            type='email'
+                            name='email'
+                            id='email'
+                            placeholder='Email'
+                            onChange={e => {
+                                let emailInput = e.target.value
+                                setNewUser(prevState => {
+                                    return { ...prevState, email: emailInput }
+                                })
+                            }}
+                        />
+                    </div>
+
+                    <div className='password-div'>
+                        <input
+                            className='password-input'
+                            type='password'
+                            name='password'
+                            id='password'
+                            placeholder='Password'
+                            onChange={e => {
+                                let passwordInput = e.target.value
+                                setNewUser(prevState => {
+                                    return { ...prevState, password: passwordInput }
+                                })
+                            }}
+                        />
+                    </div>
+
+
+                    <div className='buttons'>
+                        <div className='btn'>
+                            <button
+                                onClick={handleRegister}
+                            >Sign Up</button>
+                        </div>
+                        <div className='btn'>
+                            <button
+                            // onClick={}
+
+                            >Return</button>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    );
+
+
 }
-export default Signup;
+
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        registerUser: (user) => dispatch(registerUser(user))
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Signup)
