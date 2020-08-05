9e70b5503e119b1cabf65b405a2da5f1506067ff
KathyPuma <kathypuma96@gmail.com>
2020-08-04 22:46:20 -0400

    Create a login form that will update state and allow the user to login

--- client/src/Components/Account/Login.jsx
+++ client/src/Components/Account/Login.jsx
@@ -1,13 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from 'react'
+import { loginUser } from '../../redux/actions/userActions'
+import { CssTextField, useStyles } from '../styling/buttonStyling'
+
+
+function Login({ user, loginUser }) {
+    const classes = useStyles();
+    const [loggedInUser, setLoggedInUser] = useState({
+        username: '',
+        password: ''
+
+    })
+
+    const handleLogin = () => {
+        loginUser(loggedInUser)
+    }
+
+
+    const handleOnChange = (e) => {
+        let name = e.target.name
+        let usernameInput = e.target.value
+
+        setLoggedInUser(prevState => {
+            return { ...prevState, [name]: usernameInput }
+        })
+
+    }
 
 
 
-function Login() {
     return (
-        <div className="login-stage" >
-        </div >
-    )
+        <div className='loginPage'>
+
+            <div className='login-stage'>
+                <div className='login-form'>
+                    <div className='login'>LOGIN</div>
+
+
+                    <CssTextField
+                        className={classes.margin}
+                        label="Username"
+                        variant="outlined"
+                        id="username"
+                        name='username'
+                        onChange={handleOnChange}
+                        style={{ margin: '10px 0px 10px 0px', width: '95%', }}
+                    />
+
+
+
+                    <CssTextField
+                        className={classes.margin}
+                        label="Password"
+                        variant="outlined"
+                        id="password"
+                        name='password'
+                        onChange={handleOnChange}
+                        style={{ margin: '10px 0px 10px 0px', width: '95%', }}
+                    />
+
+                    <div >
+                        <button className='login-button-form'
+                            onClick={handleLogin}
+                        >Login</button>
+                    </div>
+
+
+                    <div className='signup-link'>
+                        New to Reddit?
+                        <span className='su-link'>SIGN UP</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+
 }
-export default Login;
+
+
+export default Login
