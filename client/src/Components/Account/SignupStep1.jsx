c1dda7e148d8866e0701e07efabc6c321428637c
KathyPuma <kathypuma96@gmail.com>
2020-08-04 22:41:53 -0400

    Remove connect, mapStateToProps and mapDispatchTo props functions in the signupStep1 form

--- client/src/Components/Account/SignupStep1.jsx
+++ client/src/Components/Account/SignupStep1.jsx
@@ -1,8 +1,4 @@
-
-
 import React from 'react'
-import { connect } from 'react-redux'
-import { registerUser } from '../../redux/actions/userActions'
 import { CssTextField, useStyles } from '../styling/buttonStyling'
 
 
@@ -20,28 +16,11 @@ function SignupStep1({ handleOnChange }) {
                 id="email"
                 name='email'
                 onChange={handleOnChange}
-                style={{margin:'0px',  width: '100%',}}
+                style={{ margin: '0px', width: '100%', }}
             />
 
         </div>
     );
-
-
 }
 
-const mapStateToProps = state => {
-    return {
-        user: state.user
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        registerUser: (user) => dispatch(registerUser(user))
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SignupStep1)
+export default SignupStep1
