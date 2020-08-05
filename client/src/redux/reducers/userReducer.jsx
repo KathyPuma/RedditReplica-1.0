97a568444b2dde782c810f3c0e5c94f3bbc1a0a1
KathyPuma <kathypuma96@gmail.com>
2020-08-04 21:58:13 -0400

    Add more cases to switch statement that will update state when a user logs in and logs out

--- client/src/redux/reducers/userReducer.jsx
+++ client/src/redux/reducers/userReducer.jsx
@@ -1,7 +1,12 @@
 import {
     REGISTER_REQUEST,
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL
 } from '../actions/actionTypes'
 
 const initialState = {
@@ -22,15 +27,42 @@ const userReducer = (state = initialState, action) => {
                 loggedIn: true,
                 user: action.payload.payload,
                 message: action.payload.message
-
             }
         case REGISTER_FAIL:
             return {
                 loggedIn: false,
                 user: [],
                 message: action.payload
-
             }
+        case LOGIN_REQUEST:
+            return {
+                ...state,
+            }
+        case LOGIN_SUCCESS:
+            return {
+                loggedIn: true,
+                user: action.payload.payload,
+                message: action.payload.message
+            }
+        case LOGIN_FAIL:
+            return {
+                loggedIn: false,
+                user: [],
+                message: action.payload
+            }
+        case LOGOUT_SUCCESS:
+            return {
+                loggedIn: true,
+                user: action.payload.payload,
+                message: action.payload.message
+            }
+        case LOGOUT_FAIL:
+            return {
+                loggedIn: false,
+                user: [],
+                message: action.payload
+            }
+
         default: return state
     }
 }
