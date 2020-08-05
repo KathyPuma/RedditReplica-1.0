ac46c28516820e41a7b5ef87f195bc53f54bb081
KathyPuma <kathypuma96@gmail.com>
2020-08-04 22:04:47 -0400

    Create Navbar that will change depending whether or not the user is logged in

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,30 +1,359 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortDown, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faReddit, faRedditAlien } from '@fortawesome/free-brands-svg-icons';
+import SignUpButton from './MaterialUiComponents/Button'
 import '../Components/Navbar.css'
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import InputOutlinedIcon from '@material-ui/icons/InputOutlined';
+import Divider from '@material-ui/core/Divider';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import customTheme from './styling/customTheme.jsx';
+import '../App.css';
 
+
+
+import { navbarStyles, StyledBadge } from './styling/navbarStyles.jsx';
+import { useRadioGroup } from '@material-ui/core';
+import { logoutUser } from '../redux/actions/userActions';
+import store from '../redux/store/store'
+import axios from 'axios';
 
 
 function Navbar() {
+    const state = store.getState();
+    const classes = navbarStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+    const history = useHistory();
+    //   const logo = require('./carry1.png')
+
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+
+
+
+    const menuId = "primary-search-account-menu";
+
+
+    const logUserOut = async () => {
+        await axios.get('/auth/logout')
+        localStorage.setItem('store', JSON.stringify({
+            user: {
+                loggedIn: false, user: [], message: ""
+            }
+        }))
+        // setTimeout(() => {
+        //     history.push('/')
+        // }, 10)
+    }
+
+
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+            className={classes.iconStyling}
+            style={{ zIndex: 9001, top: '35px' }}
+        >
+            {!state.user.loggedIn ? (
+                <div>
+                    <MenuItem onClick={handleMenuClose} className={classes.customerMenu}>
+                        {/* <Link
+              to="/login"
+              style={{
+                textDecoration: "none",
+                color: customTheme.palette.secondary.dark,
+              }}
+            >
+              Login
+            </Link> */}
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose} className={classes.customerMenu}>
+                        <Link
+                            to={`/user/ana`}
+                            style={{
+                                textDecoration: "none",
+                                color: customTheme.palette.secondary.dark,
+                            }}
+                        >
+                            Hello
+            </Link>
+                    </MenuItem>
+                    <Divider light />
+                    <MenuItem>
+                        <InputOutlinedIcon className='inputOutlinedIcon' />
+
+                        <SignUpButton buttonName={'Log In / Sign Up'}
+                            user="login"
+                            className='login-signup-button'
+                        />
+                    </MenuItem>
+
+                </div>
+            ) : (
+                    <div>
+
+                        <MenuItem onClick={handleMenuClose} className={classes.customerMenu}>
+                            <Link
+                                to={`/user/${state.user.user.username}`}
+                                style={{
+                                    textDecoration: "none",
+                                    color: "Black",
+                                    display: "flex",
+                                    columnGap: '6%'
+
+                                }}
+                            >
+                                <FontAwesomeIcon
+                                    className='dropDown-userIcon'
+                                    icon={faUser}
+                                    style={{
+                                        backgroundColor: "#808080c2",
+                                        color: "white",
+
+
+                                        padding: " 3px",
+
+
+                                        height: '.74em'
+                                    }} />
+                                My Profile
+
+                                </Link>
+                        </MenuItem>
+                        <Divider light />
+
+                        <MenuItem onClick={handleMenuClose} className={classes.customerMenu}>
+                            <Link
+                                to="/"
+                                style={{
+                                    textDecoration: "none",
+                                    color: customTheme.palette.secondary.dark,
+                                }}
+                                onClick={logUserOut}
+
+                            >
+                                Logout
+              </Link>
+                        </MenuItem>
+                    </div>
+                )}
+        </Menu>
+    );
+
+
+    const mobileMenuId = "primary-search-account-menu-mobile";
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                zIndex: 9003,
+                width: "100%",
+            }}
+        >
+
+
+
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <SearchIcon />
+                </div>
+                <InputBase
+                    placeholder="Search…"
+                    classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                    }}
+                    inputProps={{ 'aria-label': 'search' }}
+                />
+            </div>
+
+
+
+            <Divider light />
+            <MenuItem>
+
+                <SignUpButton buttonName={'Log In / Sign Up'}
+                    user="login"
+                    className={'login-button'}
+
+                />
+
+
+
+
+
+            </MenuItem>
+
+
+
+
+
+
+        </Menu>
+    );
+
     return (
-        <div className="navbar-stage"  >
-            <h1 className="youtube">reddit</h1>
+        <div
+            className={classes.grow}
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                zIndex: 1200,
+                width: "100%",
+            }}
+        >
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar>
+                    <Link
+                        to="/"
+                        style={{
+                            textDecoration: "none",
+                            color: customTheme.palette.secondary.dark,
+                        }}
+                    >
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            <Link className="reddit-link" to="/">
+                                <div className="reddit-home">
+                                    <FontAwesomeIcon
+                                        className='reddit-logo'
+                                        icon={faReddit} />
 
-            <input type="text" className = "search" placeholder="Search.."/>
-            <button>LOG IN</button>
-            <button>SIGN UP</button>
+                                </div>
+                                <p className="reddit-name">reddit</p>
+                            </Link>
+                        </Typography>
+                    </Link>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
 
-            {/* {login ? (<div></div>) : (<div></div>)} */}
-            {/* <Link to="/"  ></Link>{" "} */}
+                        {!state.user.loggedIn ? (
+                            <div className='navbarButtons'>
+                                <SignUpButton
+                                    buttonName={'LOG IN'}
+                                    user="login"
+                                    className={'login-button'}
+
+                                />
 
 
+                                <SignUpButton
+                                    buttonName={'SIGN UP'}
+                                    user='signup'
+                                    className={'signup-button'}
+
+                                />
+                            </div>
 
 
+                        ) : (<div> </div>)}
 
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            style={{ zIndex: '2' }}
+                        >
+                            <div className="dropDown-menu">
+                                <FontAwesomeIcon
+                                    className='dropDown-userIcon'
+                                    icon={faUser} />
+                                <FontAwesomeIcon
+                                    className='dropDown-arrowIcon'
+                                    icon={faSortDown} />
+
+                            </div>
+                        </IconButton>
+
+                    </div>
+
+                    <div className={classes.sectionMobile}>
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            style={{ zIndex: '2' }}
+                        >
+                            <div className="dropDown-menu">
+                                <FontAwesomeIcon
+                                    className='dropDown-userIcon'
+                                    icon={faUser} />
+                                <FontAwesomeIcon
+                                    className='dropDown-arrowIcon'
+                                    icon={faSortDown} />
+
+                            </div>
+                        </IconButton>
+
+                    </div>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
         </div >
-    )
+    );
 }
+
 export default Navbar;
-
-
 
