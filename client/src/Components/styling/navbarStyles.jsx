e58b539695c7cabeb186db76471f04ad9e568bea
KathyPuma <kathypuma96@gmail.com>
2020-08-04 23:02:03 -0400

    Remove padding in styleBadge

--- client/src/Components/styling/navbarStyles.jsx
+++ client/src/Components/styling/navbarStyles.jsx
@@ -104,6 +104,5 @@ export const StyledBadge = withStyles((theme) => ({
     right: -3,
     top: 13,
     border: `2px solid ${theme.palette.background.paper}`,
-    padding: '0 4px',
   },
 }))(Badge);
