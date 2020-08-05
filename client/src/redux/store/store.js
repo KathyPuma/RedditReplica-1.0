0e1c104e4bb63d46ef252af2a8086a2b9e58d20b
KathyPuma <kathypuma96@gmail.com>
2020-08-04 21:47:38 -0400

    Add store to local storage where store will update and persist

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,18 +1,44 @@
-import { createStore ,applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk'
 
+const saveToLocalStorage = (store) => {
 
+  try {
+    const serializedState = JSON.stringify(store)
+    localStorage.setItem('store', serializedState)
+  } catch (err) {
+    console.log("ERROR", err)
+  }
+
+}
+
+
+const loadFromLocalStorage = () => {
+
+  try {
+    const serializedState = localStorage.getItem('store')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    console.log("ERROR", err)
+    return undefined
+  }
+
+}
+
+const persistedState = loadFromLocalStorage()
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
+  persistedState,
   composeEnhancer(applyMiddleware(thunk)),
 );
 
-
-
-
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store;
