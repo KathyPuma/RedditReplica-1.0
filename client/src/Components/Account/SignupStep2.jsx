df538534b5c16eaf1438af3e767373789755a9bb
KathyPuma <kathypuma96@gmail.com>
2020-08-04 22:38:03 -0400

    Create the last step of the signup form where users put their username and password and the onchange event listner will update state in the Signup component

---
+++ client/src/Components/Account/SignupStep2.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { CssTextField, useStyles } from '../styling/buttonStyling'
+import Divider from '@material-ui/core/Divider';
+
+
+
+function SignupStep2({ handleOnChange }) {
+    const classes = useStyles();
+
+    return (
+        <div className='signupStep2Page'>
+            <h1>Choose your username</h1>
+            <h3>Your username is how other community memebers will see you. This name will be used to credit you for things you share on Reddit. What should we call you?</h3>
+
+            <Divider />
+
+            <div className='singup-button'>
+
+                <CssTextField
+                    className={classes.margin}
+                    label="Username"
+                    variant="outlined"
+                    id="username"
+                    name='username'
+                    onChange={handleOnChange}
+                    style={{ margin: '10px 0px 10px 0px', width: '95%', }}
+                />
+
+                <CssTextField
+                    className={classes.margin}
+                    label="Password"
+                    variant="outlined"
+                    id="password"
+                    name='password'
+                    onChange={handleOnChange}
+                    style={{ margin: '10px 0px 10px 0px', width: '95%', }}
+                />
+            </div>
+
+            <Divider />
+            
+        </div>
+    );
+
+
+}
+
+export default SignupStep2
