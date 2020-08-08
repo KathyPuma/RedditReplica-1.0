1af22fb9a00209bbc3f585fc51cfb51c8ff5e0e4
KathyPuma <kathypuma96@gmail.com>
2020-08-08 19:18:36 -0400

    Create query function where a new post is created

--- server/queries/comments.js
+++ server/queries/comments.js
@@ -1,4 +1,17 @@
 const db = require('../database/db')
 
+const addPost = async (obj) => {
+    const createPost = `
+    INSERT INTO subreddit_posts(subreddit_id, poster_id, title,body,photo_url)
+	VALUES($/subreddit_id/, $/poster_id/ , $/title/, $/body/ , $/photo_url/  )
+    RETURNING *
+    `
+    return await db.any(createPost, obj)
+}
 
-module.exports = {};
+
+module.exports = {
+    addPost
+};
+
+
