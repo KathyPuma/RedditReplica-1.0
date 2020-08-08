a1e1594f94ca30429911b02c0d7c12346d9fd3d7
KathyPuma <kathypuma96@gmail.com>
2020-08-08 19:22:45 -0400

    Create a post function to add a new post to the subreddit_posts table

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const commentQueries = require('../queries/comments')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/addPost', async (req, res, next) => {
+  const { subreddit_id, poster_id, title, body, photo_url } = req.body
+  try {
+    let newPost = await commentQueries.addPost({ subreddit_id, poster_id, title, body, photo_url })
+    res.json({
+      payload: newPost,
+      message: "Successfully created a post",
+      error: false
+    })
+  } catch (err) {
+    res.status(500).json({
+      payload: null,
+      message: "Failed to add a post",
+      error: true
+    })
+  }
 });
-
 module.exports = router;
