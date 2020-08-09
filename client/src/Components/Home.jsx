bb382a36f66f0a28ec6410209baa429e16942d24
KathyPuma <kathypuma96@gmail.com>
2020-08-08 20:32:07 -0400

    Import formButton and store, create handleVote function that will update when a user upvotes or downvotes a post and create styling for a paper component

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -7,22 +7,31 @@ import { homeStyle } from './styling/homeStyle'
 import Paper from '@material-ui/core/Paper';
 import Hidden from '@material-ui/core/Hidden';
 import homeBanner from '../redditImages/reddit__homeBanner.png'
+import FormButton from './MaterialUiComponents/FormButton'
+import store from '../redux/store/store'
 import './Home.css'
 
-
-
-function Home(props) {
+function Home() {
+  const state = store.getState();
 
   const [subredditsPosts, setSubredditsPosts] = useState([]);
   const classes = homeStyle();
+
   useEffect(() => {
-    const handleAllSubReddits = async () => {
-      const allSubreddits = await axios.get(`/subreddit`)
-      setSubredditsPosts(allSubreddits.data.payload)
-    }
     handleAllSubReddits()
   }, [])
 
+  const handleAllSubReddits = async () => {
+    const allSubreddits = await axios.get(`/subreddit`)
+    setSubredditsPosts(allSubreddits.data.payload)
+  }
+
+
+
+  const handleVote = async (postId, vote) => {
+    const allSubreddits = await axios.post(`/votes/updateVote`, { subreddit_posts_id: postId, voter_id: state.user.user.user_id, votes: vote })
+    handleAllSubReddits()
+  }
 
 
   return (
@@ -31,41 +40,53 @@ function Home(props) {
 
         <Hidden >
           <Paper className={classes.paper}
-            style={{ background: '#DAE0E6', boxShadow: 'none',    }}>
+            style={{
+              background: '#DAE0E6', boxShadow: 'none',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '50%'
+            }}>
 
             {subredditsPosts.map(posts => {
               return (
-                <div className="subreddit-card" key = {posts.subreddit_posts_id}>
-                  <div className='card-words'>
+                <div className="subreddit-card" key={posts.subreddit_posts_id}>
+                  <div className='home-card-container'>
+                    <div className='card-words'>
 
-                    <Link to={`/r/${posts.subreddit_name}`}>
-                      <span>r/{posts.subreddit_name} </span>
-                    </Link>
-                    <span>posted by
-                             <div>  <Link to={`/user/${posts.username}`}>
-                        <span>
-                          {posts.username}
-                        </span>
+
+                      <Link to={`/r/${posts.subreddit_name}`}>
+                        <span>r/{posts.subreddit_name} </span>
                       </Link>
-                      </div>
-                    </span>
-                  </div>
+                      <span>posted by
+                             <div>  <Link to={`/user/${posts.username}`}>
+                          <span>
+                            {posts.username}
+                          </span>
+                        </Link>
+                        </div>
+                      </span>
+
+                      <span className='card-title'>{posts.title}</span>
+                      <h2>{posts.body}</h2>
+                      <img className='subreddit-card-img' src={posts.photo_url} />
+                    </div>
 
 
-                  <span className='card-title'>{posts.title}</span>
-                  <h2>{posts.body}</h2>
-                  <img className='subreddit-card-img' src={posts.photo_url} />
+                    <div className='arrow-buttons'>
 
-                  <div>
-                    <button>
-                      <ArrowUpwardIcon />
-                    </button>
 
-                    {posts.vote_total === null ? <div> .</div> : <div>{posts.vote_total}</div>}
-                    <button>
-                      <ArrowDownwardIcon />
-                    </button>
+                      <button className='vote-button' onClick={() => { handleVote(posts.subreddit_posts_id, 1) }} >
+                        <ArrowUpwardIcon />
+                      </button>
 
+                      {posts.vote_total === null ? <div> .</div> : <div>{posts.vote_total}</div>}
+                      <button className='vote-button' onClick={() => { handleVote(posts.subreddit_posts_id, -1) }} >
+
+
+                        <ArrowDownwardIcon />
+                      </button>
+                    </div>
                   </div>
 
                 </div>
@@ -74,29 +95,41 @@ function Home(props) {
           </Paper>
         </Hidden>
 
+        {state.user.loggedIn ? (<div>
 
-        <Hidden  only="sm">
-          <Paper className={classes.paper}
-            style={{ background: '#DAE0E6', boxShadow: 'none' }}>
+          <Hidden smDown >
+            <Paper className={classes.paper}
+              style={{ background: '#DAE0E6', boxShadow: 'none', display: 'inline', padding: '12px' }}>
 
-          <div className="subreddit-card-home" style={{padding: '0px'}} >
-                  <div className='card-banner'>
-                    <img alt='reddit-banner' src={homeBanner} className = 'home-banner' />
+              <div className="subreddit-card-home">
+                <div className='card-banner'>
+                  <img alt='reddit-banner' src={homeBanner} className='home-banner' />
+                </div>
+
+                <div className='card-create-button'>
+                  <div className='community-title-div'>
+                    <span className="create-community-card"
+
+
+                    ></span>
+                    <span className="create-community-card-title">Home</span>
+
                   </div>
-                  <span className='card-title'>title</span>
+                  <span className='card-title-span'>Your personal Reddit frontpage. Come here to check in with your favorite communities.</span>
                   <div>
-                    <button>
-                    {/* <Link to={`/r/AskReddit}`}> */}
-                      CREATE COMMUNITY
-                    {/* </Link> */}
-                   
-                    </button>
+
+                    <div className='formbutton'>
+
+                      <FormButton
+                        user={state.user} />
+                    </div>
 
                   </div>
                 </div>
+              </div>
             </Paper>
-        </Hidden>
-
+          </Hidden></div>)
+          : (<></>)}
 
       </div>
 
