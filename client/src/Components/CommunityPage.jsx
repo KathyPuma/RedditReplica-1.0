d6f8005802e38fd690233d82c6cac284c09d3815
KathyPuma <kathypuma96@gmail.com>
2020-08-08 22:07:01 -0400

    Create a community page that displays all posts from a community and links to create a new post

--- client/src/Components/CommunityPage.jsx
+++ client/src/Components/CommunityPage.jsx
@@ -1,51 +1,123 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { homeStyle } from './styling/homeStyle'
+import Paper from '@material-ui/core/Paper';
+import Hidden from '@material-ui/core/Hidden';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReddit } from '@fortawesome/free-brands-svg-icons';
+import './Home.css'
 
-function CommunityPage({subreddit}) {
-
-
-
+function CommunityPage({ subreddit }) {
+    const classes = homeStyle();
     return (
-        <div className="community-stage" >
+        <div clsssName='community-cards '>
 
-         {subreddit.length !== 0 ? <div>
+            {subreddit.length !== 0 ? (<div clsssName='community-cards'>
 
 
-            <p>{subreddit[0].subreddit_name}</p>
 
- {subreddit[0].subreddit_posts.map(posts => {
-     return (
-         <div className="subreddit-card">
-        <div className = 'card-words'>
-            </div>
-           
-              <span>posted by  
-                  <div>  <Link to={`/user/${posts.username}`}>
-              <span>
-              {posts.username}
-              </span>
-              </Link>
-                  </div>         
-              </span>
+                <div style={{
+                    backgroundColor: 'white', padding: '15px', width: '36%', display: 'flex', alignItems: 'center', marginRight: '0px', borderRadius: '5px',
+                }}>
 
-         
-              <span className = 'card-title'>{posts.title}</span>
-           
-            
-             <h2>{posts.body}</h2>
-             <img className = 'subreddit-card-img' src={posts.photo_url} />
-            
-             </div>
-             )
-         })} 
 
+                    <Link className="reddit-link" to={`/r/${subreddit[0].subreddit_name}/submit`}
+                        subredditId={subreddit[0].subreddit_id}
+                    >
+                        <div className="reddit-home">
+                            <FontAwesomeIcon
+                                className='reddit-logo'
+                                icon={faReddit}
+                                style={{ color: ' #c5c5c5' }} />
+
+                        </div>
+                        <input
+                            label="body"
+                            id="body"
+                            name='body'
+                            className='input-form'
+                            type='text'
+                            style={{ borderRadius: '4px' }}
+
+                        />
+                    </Link>
 
 
 
 
+                </div>
+                <div className={classes.root} >
+                    <div className={classes.container}>
 
-         </div> : <div>      <p>{subreddit.subreddit_name}</p> </div>}
+
+                        <Hidden >
+                            <Paper className={classes.paper}
+                                style={{
+                                    background: '#DAE0E6', boxShadow: 'none',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    width: '50%'
+                                }}>
+
+
+
+                                {subreddit[0].subreddit_posts.map(posts => {
+                                    return (
+                                        <div className="subreddit-card"
+                                            style={{ width: '400px' }}
+                                            key={posts.subreddit_posts_id} >
+                                            <div className='home-card-container'
+                                                style={{ width: '-webkit-fill-available' }}>
+                                                <div className='card-words'>
+
+
+
+                                                    <Link to={`/r/${posts.subreddit_name}`}>
+                                                        <span>r/{posts.subreddit_name} </span>
+                                                    </Link>
+                                                    <span>posted by
+                       <div>  <Link to={`/user/${posts.username}`}>
+                                                            <span>
+                                                                {posts.username}
+                                                            </span>
+                                                        </Link>
+                                                        </div>
+                                                    </span>
+
+                                                    <span className='card-title'>{posts.title}</span>
+                                                    <h2>{posts.body}</h2>
+                                                    <img className='subreddit-card-img' src={posts.photo_url} />
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                    )
+                                })}
+                            </Paper>
+                        </Hidden>
+
+
+
+
+
+                    </div>
+
+                </div >
+
+
+
+
+
+            </div>) : (<div></div>)}
+
+
+
+
+
+
+
 
 
         </div >
