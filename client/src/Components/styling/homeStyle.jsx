0e2dc9a54f3f290656a9b03b9b8503e00732ee98
KathyPuma <kathypuma96@gmail.com>
2020-08-08 21:50:26 -0400

    Change width to 50% for paper component in Home.jsx

--- client/src/Components/styling/homeStyle.jsx
+++ client/src/Components/styling/homeStyle.jsx
@@ -1,24 +1,23 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const  homeStyle = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1, 
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    flex: '1 0 auto',
-    margin: theme.spacing(1),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-
-  },
-
-}));
+  export const homeStyle = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    container: {
+      display: 'flex',
+      flexWrap: 'none',
+      justifyContent: 'center',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      flex: '1 0 auto',
+      margin: theme.spacing(1),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '50%',
+    },
+  }));
