bbb60c0f7410e3e6dd61e71aac66d77af90a47bb
KathyPuma <kathypuma96@gmail.com>
2021-02-17 22:34:08 -0500

    Remove console.log

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,30 @@ import { Switch, Route, withRouter } from "react-router-dom";
 import Home from './Components/Home'
 import Navbar from './Components/Navbar'
 import Community from './Components/Community'
-import Profile from './Components/Profile'
+import ProfileLanding from './Components/Profile/ProfileLanding'
 import Comments from './Components/Comments'
-import Posts from './Components/Posts'
-import Downvoted from './Components/Downvoted'
-import Upvoted from './Components/Upvoted'
-import PostForm from './Components/PostForm'
+import Posts from './Components/Profile/Posts'
+import Downvoted from './Components/Profile/Downvoted'
+import Upvoted from './Components/Profile/Upvoted'
+// import PostForm from './Components/PostForm'
+import PostNavBar from './Components/PostForms/PostNavBar'
 import { connect } from 'react-redux'
 import { logoutUser } from './redux/actions/userActions'
 import "./App.css";
 
-
-function App({user}) {
-
+function App({ user }) {
   return (
-
     <div className="App">
-      <Navbar 
-      logoutUser = {logoutUser}
-      user = {user.user}
+      <Navbar
+        logoutUser={logoutUser}
+        user={user.user}
       />
-
       <div className="app-route">
         <Switch>
-          <Route path="/r/:community/submit/" component={PostForm} />
+          {/* <Route path="/r/:community/submit/" component={PostForm} /> */}
+          <Route path="/r/:community/submit/" component={PostNavBar} />
           <Route path="/r/:community/" component={Community} />
-          <Route path="/user/:username/" component={Profile} />
+          <Route path="/user/:username/" component={ProfileLanding} />
           <Route path="/user/:username/comments/" component={Comments} />
           <Route path="/user/:username/posts/" component={Posts} />
           <Route path="/user/:username/downvoted/" component={Downvoted} />
@@ -52,7 +50,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => {
   return {
     logoutUser: () => dispatch(logoutUser()),
-     
+
   }
 }
 
