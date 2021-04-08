9706050acaf99c250056199c9198a000573ade6d
KathyPuma <kathypuma96@gmail.com>
2021-04-08 14:28:15 -0400

    Renamed imported file and removed Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
-import Home from './Components/Home'
-import Navbar from './Components/Navbar'
-import Community from './Components/Community'
-import ProfileLanding from './Components/Profile/ProfileLanding'
-import Comments from './Components/Comments'
-import Posts from './Components/Profile/Posts'
-import Downvoted from './Components/Profile/Downvoted'
-import Upvoted from './Components/Profile/Upvoted'
-// import PostForm from './Components/PostForm'
-import PostNavBar from './Components/PostForms/PostNavBar'
-import { connect } from 'react-redux'
-import { logoutUser } from './redux/actions/userActions'
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Community from './Components/Community';
+import ProfileLanding from './Components/Profile/ProfileLanding';
+import Comments from './Components/Comments';
+import Posts from './Components/Profile/Posts';
+import Downvoted from './Components/Profile/Downvoted';
+import Upvoted from './Components/Profile/Upvoted';
+import PostNavbar from './Components/PostForms/PostNavbar';
+import { connect } from 'react-redux';
+import { logoutUser } from './redux/actions/userActions';
 import "./App.css";
 
 function App({ user }) {
@@ -23,8 +22,7 @@ function App({ user }) {
       />
       <div className="app-route">
         <Switch>
-          {/* <Route path="/r/:community/submit/" component={PostForm} /> */}
-          <Route path="/r/:community/submit/" component={PostNavBar} />
+          <Route path="/r/:community/submit/" component={PostNavbar} />
           <Route path="/r/:community/" component={Community} />
           <Route path="/user/:username/" component={ProfileLanding} />
           <Route path="/user/:username/comments/" component={Comments} />
