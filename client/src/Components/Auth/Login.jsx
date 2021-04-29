6e50cdc69d5557e8891c431ffe5fa197f041327b
KathyPuma <kathypuma96@gmail.com>
2021-04-28 21:53:22 -0400

    Added input type to input forms

--- client/src/Components/Auth/Login.jsx
+++ client/src/Components/Auth/Login.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useFormFields } from '../Helpers/HelperFunctions'
-import FormInput from '../FormInput'
+import React from "react"
+import { useFormFields } from "../Helpers/HelperFunctions"
+import FormInput from "../FormInput"
 
 function Login({ loginUser, SetLoginAction }) {
     const [loggedInUser, setLoggedInUser] = useFormFields({
@@ -13,40 +13,43 @@ function Login({ loginUser, SetLoginAction }) {
     }
 
     return (
-        <div className='loginPage'>
+        <div className="loginPage">
 
-            <div className='login-stage'>
-                <div className='login-form'>
-                    <div className='login'>LOGIN</div>
+            <div className="login-stage">
+                <div className="login-form">
+                    <div className="login">LOGIN</div>
 
 
                     <FormInput
                         inputLabel="Username"
                         inputName="username"
+                        inputType="text"
                         inputValue={loggedInUser.username}
                         updateInput={setLoggedInUser}
-                        style={{ margin: '10px 0px 10px 0px', width: '95%', }}
+                        style={{ margin: "10px 0px 10px 0px", width: "95%", }}
                     />
 
 
                     <FormInput
                         inputLabel="Password"
                         inputName="password"
+                        inputType="password"
                         inputValue={loggedInUser.password}
                         updateInput={setLoggedInUser}
-                        style={{ margin: '10px 0px 10px 0px', width: '95%', }}
+
+                        style={{ margin: "10px 0px 10px 0px", width: "95%", }}
                     />
 
                     <div >
-                        <button className='login-button-form'
+                        <button className="login-button-form"
                             onClick={handleLogin}
                         >LOG IN</button>
                     </div>
 
 
-                    <div className='signup-link'>
+                    <div className="signup-link">
                         New to Reddit?
-                        <span className='su-link' onClick={() => SetLoginAction('SignUp')}>SIGN UP</span>
+                        <span className="su-link" onClick={() => SetLoginAction("SignUp")}>SIGN UP</span>
                     </div>
                 </div>
             </div>
--- client/src/Components/FormInput.jsx
+++ client/src/Components/FormInput.jsx
@@ -1,14 +1,15 @@
 
-import React from 'react'
-import { CssTextField, useStyles } from './styling/InputStyling'
+import React from "react"
+import { CssTextField, useStyles } from "./styling/InputStyling"
 
 
-export default function FormInput({ inputLabel, inputName, inputValue, updateInput }) {
+export default function FormInput({ inputLabel, inputName, inputType, inputValue, updateInput }) {
     const inputStyle = useStyles();
 
     return (
         <CssTextField
             className={inputStyle.margin}
+            type={inputType}
             label={inputLabel}
             variant="outlined"
             id={inputName}
